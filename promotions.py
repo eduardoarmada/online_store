b7da2058b2b2e8a4c0d0c3459fd65be4de1fe78b
eduardoarmada <eduardo.jose.armada@gmail.com>
2023-06-05 00:40:21 +0200

    Final commit

--- promotions.py
+++ promotions.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class Promotions(ABC):
+    """Creates a discount on the products, class base for creating different kinds of discounts"""
     def __init__(self, promotion_name):
         self.promotion_name = promotion_name
 
@@ -11,6 +12,8 @@ class Promotions(ABC):
 
 
 class PercentageDiscount(Promotions):
+    """Creates a discount that consist of the decrease of the total value of the products and the quantities by the
+    indicated percentage"""
     def __init__(self, promotion_name, percentage):
         super().__init__(promotion_name)
         self.percentage = percentage
@@ -23,6 +26,7 @@ class PercentageDiscount(Promotions):
 
 
 class SecondHalfPricePromotion(Promotions):
+    """Creates a discount that consist of each 1 product bought, the next one is 50% off"""
     def apply_promotion(self, product, quantity):
         return (product.price * (quantity // 2)) / 2 + product.price * (quantity - (quantity // 2))
 
@@ -31,6 +35,7 @@ class SecondHalfPricePromotion(Promotions):
 
 
 class Buy2Get1Free(Promotions):
+    """Creates a discount that consist of each 2 products bought, 1 is free."""
     def apply_promotion(self, product, quantity):
         return product.price * (quantity // 3) * 2 + product.price * quantity % 3
 
